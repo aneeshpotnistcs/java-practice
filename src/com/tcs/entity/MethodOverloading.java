f1391ef23234b179251646a6bdfa3a036cd6fe10
aneeshpotnistcs <aneeshpotnistcs@gmail.com>
2021-08-10 09:55:47 +0000

    method overriding

--- src/com/tcs/entity/MethodOverloading.java
+++ src/com/tcs/entity/MethodOverloading.java
@@ -2,17 +2,16 @@ package com.tcs.entity;
 
 public class MethodOverloading {
 	public static void main(String[] args) {
-		User1 user1 = new User1();
+		User1 user1 = new Employee1();
 		user1.work();
 		user1.work(4);
 		user1.work("4");
 	}
-	
-
 }
 
-class User1{
+class User1{//super class
 	public void work() {
+		
 		System.out.println("user is working");
 	}
 	
@@ -21,5 +20,13 @@ class User1{
 	}
 	public void work(String duration) {
 		System.out.println("user is working for " + duration + " hours");
+	}
+}
+
+class Employee1 extends User1{//sub class
+	@Override
+	public void work() {
+		super.work();
+		System.out.println("Employee is working");
 	}
 }
