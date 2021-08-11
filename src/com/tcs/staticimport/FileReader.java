a5830a265a0ec66d2e22e79cce6bbafbf82c1eca
aneeshpotnistcs <aneeshpotnistcs@gmail.com>
2021-08-11 06:40:21 +0000

    static import

---
+++ src/com/tcs/staticimport/FileReader.java
@@ -0,0 +1,13 @@
+package com.tcs.staticimport;
+
+//import static java.util.Arrays.*;
+
+import java.util.Arrays;
+
+public class FileReader {
+public static void main(String[] args) {
+	int[] numbers = {4,5,6,7,9};
+//	sort(numbers); //works with static import
+	Arrays.sort(numbers);//works without static import
+}
+}
